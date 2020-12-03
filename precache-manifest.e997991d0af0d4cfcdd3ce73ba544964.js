f8cbff95a61f22d03af219af54e47e758840884d
videsh <videsh.ghodarop@iaugmentor.com>
2020-12-03 14:00:17 +0530

    Updates

--- precache-manifest.e997991d0af0d4cfcdd3ce73ba544964.js
+++ precache-manifest.e997991d0af0d4cfcdd3ce73ba544964.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1cd2f3aca9f18487b8e3cfa3a34ecb60",
+    "revision": "fc632bac7e1f8c4c6cc166599125e172",
     "url": "/category-assignment/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/category-assignment/static/css/2.d88ce3aa.chunk.css"
   },
   {
-    "revision": "a27b7248c206769e5292",
+    "revision": "8c250fdfb5b72588a456",
     "url": "/category-assignment/static/css/main.075dee18.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/category-assignment/static/js/2.ac3612e7.chunk.js"
   },
   {
-    "revision": "a27b7248c206769e5292",
-    "url": "/category-assignment/static/js/main.27b13704.chunk.js"
+    "revision": "8c250fdfb5b72588a456",
+    "url": "/category-assignment/static/js/main.4c80303d.chunk.js"
   },
   {
     "revision": "8801686fca60cbcd46c2",
